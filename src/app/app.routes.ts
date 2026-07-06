403fa182f610de47816a8413a05e868884536acc
SANDY <dtena90@gmail.com>
2026-07-06 13:33:49 +0200

    feat: add welcome route and global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Welcome } from './pages/welcome/welcome';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Welcome,
+    title: 'SecureBoard · Security Operations Center',
+  },
+
+  // Si alguien escribe una ruta que todavía no existe,
+  // vuelve a la pantalla Welcome.
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
